50a4f3c6231fbf36a5e9535a97df4533e59d1ea4
anh123minh <phamanhminh231@gmail.com>
2018-11-11 02:37:33 +0300

    create

---
+++ OnlineShop/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineShop.Areas.Admin.Models;
+using Models;
+using OnlineShop.Areas.Admin.Code;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(LoginModel model)
+        {
+            var result = new AccountModel().Login(model.UserName, model.Password);
+            if (result && ModelState.IsValid)
+            {
+                SessionHelper.SetSession(new UserSession(){UserName = model.UserName});
+                return RedirectToAction("Index", "HomeAdmin");
+            }
+            else
+            {
+                ModelState.AddModelError("","Ten dang nhap hoac mat khau khong dung.");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ SQLQuery/SQLQuery1.sql
@@ -0,0 +1,2 @@
+/*alter table [OnlineShop1].[dbo].[Account] alter column UserName nvarchar(50) NOT NULL*/
+alter table Account add primary key (UserName)
