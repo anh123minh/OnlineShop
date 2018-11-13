70b6e735cfa024653247cdc853e85d8a780b5e58
anh123minh <phamanhminh231@gmail.com>
2018-11-13 17:02:43 +0300

    xong bai 7
    chua set Authorize cho Category/Index

--- Models/Framework/OnlineShopDbContext.cs
+++ Models/Framework/OnlineShopDbContext.cs
@@ -13,9 +13,30 @@ namespace Models.Framework
         }
 
         public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Account>()
+                .Property(e => e.UserName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Account>()
+                .Property(e => e.Password)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Category>()
+                .Property(e => e.Alias)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Alias)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Product>()
+                .Property(e => e.Price)
+                .HasPrecision(18, 0);
         }
     }
 }
