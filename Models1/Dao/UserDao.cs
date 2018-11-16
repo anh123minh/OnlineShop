4cf0c13613fbad0f9a37b9b066f17e2000aa8d4e
anh123minh <phamanhminh231@gmail.com>
2018-11-17 01:03:45 +0300

    xong bai 12

---
+++ Models1/Dao/UserDao.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Models1.EF;
+
+namespace Models1.Dao
+{
+    public class UserDao
+    {
+        private OnlineShopDbContext db = null;
+
+        public UserDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+
+        public long Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        public bool Login(string userName, string password)
+        {
+            var result = db.Users.Count(x => x.UserName == userName && x.Password == password);
+            if (result > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
